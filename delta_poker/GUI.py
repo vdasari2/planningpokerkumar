6986c48dc3a3aada6d10bdd721e150621590494a
Vishnu Preetham Reddy Dasari <vdasari2@asu.edu>
2022-02-10 22:53:16 -0700

    Story #20 :: Task#22 :: GUI HomeScreen for PlanningPoker
    Changes made to the earlier code

--- delta_poker/GUI.py
+++ delta_poker/GUI.py
@@ -7,21 +7,16 @@ def openNewWindow():
 	newWindow.title("New Window")
 	newWindow.geometry("400x400")
 	Label(newWindow,
-		text ="Team Members:").pack()
-	Label(newWindow,
-		text ="Vishnu Preetham Reddy Dasari").pack()
-	Label(newWindow,
-		text ="Sai Teja Reddy Cheela").pack()
-	Label(newWindow,
-		text ="Praneeth Sai Kumar Reddy Kallam").pack()
-	Label(newWindow,
-		text ="Janardhan Reddy Bommireddy").pack()
-
-
-
+		text ="Team Members: \n Vishnu Preetham Reddy Dasari \n Sai Teja Reddy Cheela \n Praneeth Sai Reddy Kallam \n Janardhan Reddy Bommireddy").pack()
+	
 label = Label(master,
 			text =" Welcome to the Project PlanningPoker\n This is the basic GUI HomeScreen for our project")
 label.pack(pady = 10)
+
+btn = Button(master,
+			text ="Click here to see the Collaboraters ",
+			command = openNewWindow)
+btn.pack(pady = 10)
 
 btn = Button(master,
 			text ="Click here to see the Collaboraters ",
