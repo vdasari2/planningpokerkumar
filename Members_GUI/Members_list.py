8dbefcad3cd79904f3907a7a76ebe026a641e7eb
Vishnu Preetham Reddy Dasari <vdasari2@asu.edu>
2022-02-22 20:30:41 -0700

    Update Members_list.py

--- Members_GUI/Members_list.py
+++ Members_GUI/Members_list.py
@@ -1,17 +1,13 @@
+# Import Module
 from tkinter import *
-import delta_cli
+from turtle import width
 
-object = delta_cli.MyPrompt()
 # Function will remove selected Listbox items
 def remove_item():
 	selected_checkboxs = listbox.curselection()
 
 	for selected_checkbox in selected_checkboxs[::-1]:
 		listbox.delete(selected_checkbox)
-
-def refresh():
-       root.destroy()
-       exec("Members_list.py",globals())
 
 # Create Object
 root = Tk()
@@ -24,21 +20,13 @@ listbox = Listbox(root,height = 20, width = 20, selectmode=MULTIPLE)
 listbox.pack()
 
 # Listbox Items List
-items = object.do_current_players(inp="current_players")
-print(type(items))
+items = ["Vishnu", "Praneeth", "Saiteja", "Janardhan"]
 
 # Iterate Through Items list
-if items is None:
-    items = []
-else:
-    for item in items:
-        listbox.insert(END, item)
+for item in items:
+	listbox.insert(END, item)
 
-
-photo = PhotoImage(file = r"/Users/praneethkallam/SER-516-Project/delta_poker/reloading.png")
-photoimage = photo.subsample(3, 3)
-a = Button(root, text="Refresh", image = photoimage, compound = LEFT, command = refresh).pack()
-b = Button(root, text="delete",command=remove_item).pack()
+Button(root, text="delete",bg="green",command=remove_item).pack()
 
 # Execute Tkinter
 root.mainloop()
