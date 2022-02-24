aabae4dcb032b91ceef07104d2de4b6a1da48804
Praneeth-kallam <90414297+Praneeth-kallam@users.noreply.github.com>
2022-02-23 23:36:49 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+from http import server
+from re import T
+import threading
 import tkinter
 from subprocess import Popen
 
@@ -8,19 +11,22 @@ top = tkinter.Tk()
 top.title('MapAwareness')
 top.geometry('400x400') # Size 200, 200
 
-def start():
-    import os
-    os.system("uvicorn delta_poker:app --host=0.0.0.0")
+def poker_server():
+    global server
+    def starter():
+        import os
+        os.system("uvicorn delta_poker:app --host=0.0.0.0")
+
+    server = threading.Thread(target=starter, daemon=True)
+    server.start()
 
 
 def stop():
     print ("Stop")
     top.destroy()
 
-startButton = tkinter.Button(top, height=2, width=20, text ="Start", 
-command = start)
-stopButton = tkinter.Button(top, height=2, width=20, text ="Stop", 
-command = stop)
+startButton = tkinter.Button(top, height=2, width=20, text ="Start", command = poker_server)
+stopButton = tkinter.Button(top, height=2, width=20, text ="Stop", command = stop)
 
 startButton.pack()
 stopButton.pack()
